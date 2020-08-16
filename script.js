547ba288a4c4ef5be3982f3b0be9dad4133dec61
Sabina <sabina.abbasova2909@gmail.com>
2020-08-17 01:31:20 +0400

    added nav interactions

--- script.js
+++ script.js
@@ -1,62 +1,121 @@
 // NAVIGATION
 // Toggle menu
-const hamburgerMenuIcon = document.querySelector('header.big-screen .menu-btn .menu-icon');
+const hamburgerMenuButton = document.querySelector('header.big-screen .menu-btn');
 const menuContainer = document.querySelector('header.big-screen nav');
+const menuIcon = document.querySelector('header.big-screen .menu-btn .menu-icon');
+const dot1 = document.querySelector('header.big-screen .menu-btn .menu-icon div.one');
+const dot2 = document.querySelector('header.big-screen .menu-btn .menu-icon div.two');
+const dot3 = document.querySelector('header.big-screen .menu-btn .menu-icon div.three');
+const dot4 = document.querySelector('header.big-screen .menu-btn .menu-icon div.four');
+const dot5 = document.querySelector('header.big-screen .menu-btn .menu-icon div.five');
+const dot6 = document.querySelector('header.big-screen .menu-btn .menu-icon div.six');
+const dot7 = document.querySelector('header.big-screen .menu-btn .menu-icon div.seven');
+const dot8 = document.querySelector('header.big-screen .menu-btn .menu-icon div.eight');
+const dot9 = document.querySelector('header.big-screen .menu-btn .menu-icon div.nine');
 
 
-// let isMenuOpened = false;
+let isMenuOpened = false;
 
-// hamburgerMenuIcon.addEventListener("click", checkMenuIsOpened);
-// function checkMenuIsOpened(){
-//     if(isMenuOpened === false){
-//         openMenu()
-//     } else if(isMenuOpened === true){
-//         closeMenu()
-//         // console.log("clicked")
-//     }
-// }
-// function openMenu(){
-//     isMenuOpened = true;
-//     // hamburgerMenuIcon.src = '../assets/images/close-icon-blue.svg'
-//     hamburgerMenuIcon.alt = 'close icon'
-//     menuContainer.style.transform = "translate(-50%, 0px)"
+hamburgerMenuButton.addEventListener("click", checkMenuIsOpened);
+function checkMenuIsOpened(){
+    if(isMenuOpened === false){
+        openMenu()
+    } else if(isMenuOpened === true){
+        closeMenu()
+        // console.log("clicked")
+    }
+}
+function openMenu(){
+    isMenuOpened = true;
+    dot1.style.display = "none"
+    setTimeout(() => {
+      dot2.style.display = "none"
+
+      setTimeout(() => {
+        dot3.style.display = "none"
+
+        setTimeout(() => {
+          dot4.style.display = "none"
+
+          setTimeout(() => {
+            dot5.style.display = "none"
+
+            setTimeout(() => {
+              dot6.style.display = "none"
+
+              setTimeout(() => {
+                dot7.style.display = "none"
+
+                setTimeout(() => {
+                  dot8.style.display = "none"
+
+                  menuContainer.style.display = "flex"
+                  setTimeout(() => {
+                    menuContainer.style.transform = "translate(-50%, 0px)"
+                  }, 50)
+
+                  setTimeout(() => {
+                    dot9.style.display = "none"
+                    
+                    setTimeout(() => {
+                      menuIcon.classList.add('clicked')
+                    }, 50)
+
+                  }, 50)
+                }, 50)
+              }, 50)
+            }, 50)
+          }, 50)
+        }, 50)
+      }, 50)
+    }, 50)
+
+
+
    
-// }
-// function closeMenu(){
-//     isMenuOpened = false;
-//     // hamburgerMenuIcon.src = '../assets/images/hamburger-icon.svg'
-//     menuContainer.style.transform = "translate(-50%, -100vh)"
-//     hamburgerMenuIcon.alt = 'hamburger icon'
-  
-// }
+}
+function closeMenu(){
+    isMenuOpened = false;
+    menuContainer.style.transform = "translate(-50%, -1200px)"
+    menuIcon.classList.remove('clicked')
+    setTimeout(() => {
+      dot9.style.display = "block"
 
-// // NAVIGATION
-// const navButton = document.querySelector("header > div.nav-button");
-// const navMenu = document.querySelector("header nav.small-screen");
-// const menuItems = document.querySelector("header nav.small-screen ul");
-// const menuLinks = document.querySelectorAll("header nav.small-screen ul li > .items a");
-// const menuLinkContactUs = document.querySelector("header nav.small-screen ul li.contact-us a");
-// const patternSmScreen = document.querySelector(
-//   "body > header > img.pattern-sm-screen"
-// );
-hamburgerMenuIcon.addEventListener("click", openMenu);
-// menuLinks.forEach(menuLink => {
-//   menuLink.addEventListener("click", closeMenu);
-// });
-// menuLinkContactUs.addEventListener("click", () => {
-//   setTimeout(() => {
-//     closeMenu();
-//   }, 680)
-// });
-function openMenu() {
-    // navButton.classList.toggle("close");
-    menuContainer.classList.toggle("open");
-    // menuItems.classList.toggle("menu-items-appear");
-    // patternSmScreen.classList.toggle("show-pattern");
-  }
-//   function closeMenu() {
-//     navButton.classList.remove("close");
-//     navMenu.classList.remove("open");
-//     menuItems.classList.remove("menu-items-appear");
-//     patternSmScreen.classList.remove("show-pattern");
-//   }
+      setTimeout(() => {
+        dot8.style.display = "block"
+
+        setTimeout(() => {
+          dot7.style.display = "block"
+
+          setTimeout(() => {
+            dot6.style.display = "block"
+
+            setTimeout(() => {
+              dot5.style.display = "block"
+
+              setTimeout(() => {
+                dot4.style.display = "block"
+
+                setTimeout(() => {
+                  dot3.style.display = "block"
+
+                  setTimeout(() => {
+                    dot2.style.display = "block"
+
+                    setTimeout(() => {
+                      dot1.style.display = "block"
+                    },50)
+                  },50)
+                },50)
+              },50)
+            },50)
+          },50)
+        },50)
+      },50)
+    },100)
+    setTimeout(() => {
+      menuContainer.style.display = "none"
+    }, 800)
+
+  
+}
